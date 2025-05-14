c77a0b72bc4551c7ad86022b33fe9ff40ef39a65
Yann Roche <yannou20@MacBook-Pro-de-Yann.local>
2025-05-14 15:31:55 +0200

    adding file

---
+++ src/pymodaq_plugins_synktek/daq_move_plugins/daq_move_MCL1540_Lockin.py
@@ -0,0 +1,217 @@
+from typing import Union, List, Dict
+from pymodaq.control_modules.move_utility_classes import (DAQ_Move_base, comon_parameters_fun,
+                                                          main, DataActuatorType, DataActuator)
+
+from pymodaq_utils.utils import ThreadCommand
+from pymodaq_gui.parameter import Parameter
+
+from MCL import MCL
+LOCKIN_CHANNELS = ['L1', 'L2']
+
+
+class DAQ_Move_MCL1540_Lockin(DAQ_Move_base):
+    """ Instrument plugin class for an actuator.
+
+    This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Move module through inheritance via
+    DAQ_Move_base. It makes a bridge between the DAQ_Move module and the Python wrapper of a particular instrument.
+
+    TODO Complete the docstring of your plugin with:
+        * The set of controllers and actuators that should be compatible with this instrument plugin.
+        * With which instrument and controller it has been tested.
+        * The version of PyMoDAQ during the test.
+        * The version of the operating system.
+        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
+
+    Attributes:
+    -----------
+    controller: object
+        The particular object that allow the communication with the hardware, in general a python wrapper around the
+         hardware library.
+
+    # TODO add your particular attributes here if any
+
+    """
+    is_multiaxes = False  # TODO for your plugin set to True if this plugin is controlled for a multiaxis controller
+    _axis_names: Union[List[str], Dict[str, int]] = [
+        'Axis1', 'Axis2']  # TODO for your plugin: complete the list
+    # TODO for your plugin: put the correct unit here, it could be
+    _controller_units: Union[str, List[str]] = 'Hz'
+    # TODO  a single str (the same one is applied to all axes) or a list of str (as much as the number of axes)
+    # TODO replace this by a value that is correct depending on your controller
+    _epsilon: Union[float, List[float]] = 0.01
+    # TODO it could be a single float of a list of float (as much as the number of axes)
+    # wether you use the new data style for actuator otherwise set this
+    data_actuator_type = DataActuatorType.DataActuator
+    # as  DataActuatorType.float  (or entirely remove the line)
+
+    params = [{'title': 'Ip address', 'name': 'ip', 'type': 'str'},
+              
+              {'title': 'Lockin channel', 'name': 'lockinchannel',
+                  'type': 'list', 'limits': LOCKIN_CHANNELS},
+
+              {'title': 'Input mode', 'name': 'imode', 'type': 'list',
+               'limits': ['Voltage']},
+
+              {'title': 'Full-scale sensitivity', 'name': 'sensitivity',
+               'type': 'list', 'limits': [10, 1, 0.1]},
+
+              {'title': 'Voltage (V)', 'name': 'voltage', 'type': 'float',
+               'limits': [0, 5], 'value': 1e-6},
+
+              {'title': 'Frequency (Hz)', 'name': 'frequency', 'type': 'float',
+               'limits': [0, 30], 'value': 13},
+
+              ] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon)
+    # _epsilon is the initial default value for the epsilon parameter allowing pymodaq to know if the controller reached
+    # the target value. It is the developer responsibility to put here a meaningful value
+
+    def ini_attributes(self):
+        self.controller: MCL = None
+        pass
+
+    def get_actuator_value(self):
+        """Get the current value from the hardware with scaling conversion.
+
+        Returns
+        -------
+        float: The position obtained after scaling conversion.
+        """
+        freq = DataActuator(data=self.controller.config.frequency_1.frequency)
+        freq = self.get_position_with_scaling(freq)
+        return freq
+
+
+    #def user_condition_to_reach_target(self) -> bool:
+        """ Implement a condition for exiting the polling mechanism and specifying that the
+        target value has been reached
+
+       Returns
+        -------
+        bool: if True, PyMoDAQ considers the target value has been reached
+        """
+        # TODO either delete this method if the usual polling is fine with you, but if need you can
+        #  add here some other condition to be fullfilled either a completely new one or
+        #  using or/and operations between the epsilon_bool and some other custom booleans
+        #  for a usage example see DAQ_Move_brushlessMotor from the Thorlabs plugin
+        return True
+
+    def close(self):
+        """Terminate the communication protocol"""
+        self.controller.disconnect()
+
+
+    def commit_settings(self, param: Parameter):
+        """Apply the consequences of a change of value in the detector settings
+
+        Parameters
+        ----------
+        param: Parameter
+            A given parameter (within detector_settings) whose value has been changed by the user
+        """
+        # TODO for your custom plugin
+        if param.name() == 'axis':
+            self.axis_unit = self.controller.your_method_to_get_correct_axis_unit()
+            # do this only if you can and if the units are not known beforehand, for instance
+            # if the motors connected to the controller are of different type (mm, µm, nm, , etc...)
+            # see BrushlessDCMotor from the thorlabs plugin for an exemple
+
+        elif param.name() == "voltage":
+            self.controller.config.amplitude_1.amplitude = param.value()
+        
+        elif param.name() == "frequency":
+            self.controller.config.amplitude_1.amplitude = param.value()
+    
+        else:
+            pass
+
+    def ini_stage(self, controller=None):
+        """Actuator communication initialization
+
+        Parameters
+        ----------
+        controller: (object)
+            custom object of a PyMoDAQ plugin (Slave case). None if only one actuator by controller (Master case)
+
+        Returns
+        -------
+        info: str
+        initialized: bool
+            False if initialization failed otherwise True
+        """
+
+        self.ini_stage_init(slave_controller=controller)
+
+        if self.is_master:  # is needed when controller is master
+            try:
+                self.controller = MCL()  
+                self.controller.connect(mcl_ip=self.settings['ip'])
+                info = "Connected to Lockin"
+                initialized = True
+            except:
+                initialized = False
+                info = "connection failed"
+
+        return info, initialized
+
+    def move_abs(self, value: DataActuator):
+        """ Move the actuator to the absolute target defined by value
+
+        Parameters
+        ----------
+        value: (float) value of the absolute target positioning
+        """
+
+        # if user checked bounds, the defined bounds are applied here
+        value = self.check_bound(value)
+        self.target_value = value
+        # apply scaling if the user specified one
+        value = self.set_position_with_scaling(value)
+        # TODO for your custom plugin
+        raise NotImplemented  # when writing your own plugin remove this line
+        self.controller.your_method_to_set_an_absolute_value(
+            value.value())  # when writing your own plugin replace this line
+        self.emit_status(ThreadCommand(
+            'Update_Status', ['Some info you want to log']))
+
+    def move_rel(self, value: DataActuator):
+        """ Move the actuator to the relative target actuator value defined by value
+
+        Parameters
+        ----------
+        value: (float) value of the relative target positioning
+        """
+        value = self.check_bound(
+            self.current_position + value) - self.current_position
+        self.target_value = value + self.current_position
+        value = self.set_position_relative_with_scaling(value)
+
+        # TODO for your custom plugin
+        raise NotImplemented  # when writing your own plugin remove this line
+        self.controller.your_method_to_set_a_relative_value(
+            value.value())  # when writing your own plugin replace this line
+        self.emit_status(ThreadCommand(
+            'Update_Status', ['Some info you want to log']))
+
+    def move_home(self):
+        """Call the reference method of the controller"""
+
+        # TODO for your custom plugin
+        raise NotImplemented  # when writing your own plugin remove this line
+        # when writing your own plugin replace this line
+        self.controller.your_method_to_get_to_a_known_reference()
+        self.emit_status(ThreadCommand(
+            'Update_Status', ['Some info you want to log']))
+
+    def stop_motion(self):
+        """Stop the actuator and emits move_done signal"""
+
+        # TODO for your custom plugin
+        raise NotImplemented  # when writing your own plugin remove this line
+        # when writing your own plugin replace this line
+        self.controller.your_method_to_stop_positioning()
+        self.emit_status(ThreadCommand(
+            'Update_Status', ['Some info you want to log']))
+
+
+if __name__ == '__main__':
+    main(__file__)
